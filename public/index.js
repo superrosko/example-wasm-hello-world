144ae90a54173ddfddf1c0f586ddb0a0c6d40e3a
Dmitriy Bespalov <dbespalov.rosko@gmail.com>
2020-05-03 01:19:20 +0300

    style(js): code style

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
-const Module = require("./hello_world.wasm.js")
-const {performance} = require('perf_hooks');
+const Module = require("./hello_world.wasm.js");
+const { performance } = require("perf_hooks");
 
-Module['onRuntimeInitialized'] = onRuntimeInitialized;
+Module["onRuntimeInitialized"] = onRuntimeInitialized;
 function onRuntimeInitialized() {
-    const t0 = performance.now();
-    const getHelloMessage = Module.cwrap('getHelloMessage', 'string', []);
-    const result = getHelloMessage();
-    const t1 = performance.now();
+  const t0 = performance.now();
+  const getHelloMessage = Module.cwrap("getHelloMessage", "string", []);
+  const result = getHelloMessage();
+  const t1 = performance.now();
 
-    console.log(`Result : "${result}"`)
-    console.log("Run time " + (t1 - t0) + " milliseconds.")
+  console.log(`Result : "${result}"`);
+  console.log("Run time " + (t1 - t0) + " milliseconds.");
 }
